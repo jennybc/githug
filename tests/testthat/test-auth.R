bbd65bf731d6b286f15da06533d3bf6714ed5f7b
jennybc <jenny@stat.ubc.ca>
2016-01-11 15:55:31 -0800

    gh_pat
    go ahead and import gh so help links work, even though i get NOTE about not using it yet

---
+++ tests/testthat/test-auth.R
@@ -0,0 +1,49 @@
+context("auth")
+
+test_that("we can retrieve github PAT from env var", {
+
+  ## default to GITHUB_PAT
+  withr::with_envvar(
+    c("GITHUB_PAT" = "GITHUB_PAT", "GITHUB_TOKEN" = "GITHUB_TOKEN"), {
+      pat <- gh_pat()
+    })
+  expect_identical(pat, c("GITHUB_PAT" = "GITHUB_PAT"))
+
+  ## if no GITHUB_PAT, then GITHUB_TOKEN
+  withr::with_envvar(
+    c("GITHUB_PAT" = NA, "GITHUB_TOKEN" = "GITHUB_TOKEN"), {
+      pat <- gh_pat()
+    })
+  expect_identical(pat, c("GITHUB_TOKEN" = "GITHUB_TOKEN"))
+
+  ## if user specifies, that takes precedence
+  withr::with_envvar(
+    c("GITHUB_PAT" = "GITHUB_PAT", "GITHUB_TOKEN" = "GITHUB_TOKEN",
+      "a" = "a"), {
+        pat <- gh_pat("a")
+      })
+  expect_identical(pat, c("a" = "a"))
+
+})
+
+test_that("we don't retrieve github PAT when we should not", {
+
+  ## none of defaults exist?
+  withr::with_envvar(c("GITHUB_PAT" = NA, "GITHUB_TOKEN" = NA), {
+    pat <- gh_pat()
+  })
+  expect_identical(pat, "")
+
+  ## no defaults and user-specified env var doesn't exist either
+  withr::with_envvar(c("GITHUB_PAT" = NA, "GITHUB_TOKEN" = NA), {
+    pat <- gh_pat("a")
+  })
+  expect_identical(pat, "")
+
+  ## defaults exist but user is asking for something else
+  withr::with_envvar(c("GITHUB_PAT" = "GITHUB_PAT", "GITHUB_TOKEN" = "GITHUB_TOKEN"), {
+    pat <- gh_pat("a")
+  })
+  expect_identical(pat, "")
+
+})
